9ef82207605fdc830d81b28547b006852738341b
Lukas Horak <lukasbhorak@gmail.com>
2019-06-11 15:51:54 -0400

    implemented character counting for new Tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-  console.log("Gimme the Jeff!");
   $('.new-tweet textarea').on("input", function(){
     const inputLength = $(this).val().length;
     const charsLeft = 140 - inputLength;
-    $(this).siblings('.counter')[0].innerHTML = charsLeft
+    
     // Red for over, yellow for < 10
-    const textColor = charsLeft < 0 ? "red" : charsLeft < 10 ? "goldenrod" : "";
-    $(this).siblings('.counter').css("color", textColor);
+    const status = charsLeft < 0 ? "over" : charsLeft < 10 ? "warn" : "under";
+    $(this).siblings('.counter').text(charsLeft).attr("data-status", status);
   })
 });
