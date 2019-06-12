e3c7d904541a6ef0828d38648798c4f55e43e3b2
Lukas Horak <lukasbhorak@gmail.com>
2019-06-12 17:48:03 -0400

    changed color scheme; implemented compose button

--- public/scripts/reset.js
+++ public/scripts/reset.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
   $('#nav-bar .logo, .header').on("click", function(){
     $('html, body').animate({scrollTop:0}, 'slow');
   })
+
+  $('.compose').on("click", function(){
+    $('html, body').animate({
+      scrollTop: $(".new-tweet").offset().top - 95},
+      'slow');
+    $(".new-tweet textarea").focus();
+  })
 })
