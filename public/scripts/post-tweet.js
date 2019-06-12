9f03adfe26d968a562bb9394c347c842142ce18a
Lukas Horak <lukasbhorak@gmail.com>
2019-06-12 16:37:13 -0400

    clear submission area on tweet submission

--- public/scripts/post-tweet.js
+++ public/scripts/post-tweet.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
     const tweetLength = $button.siblings("textarea").val().length;
     let error = (tweetLength > 140 ? "Too much Jeffness for one tweet. We can't handle that much Jeffness!" : tweetLength === 0 ? "There's no Jeffness here!" : undefined);
     if(error) {
-      alert(error)
+
+      alert(error);
 ;    }
     else{
       $.ajax({
@@ -14,6 +15,8 @@ $(document).ready(function() {
       url: '/tweets/',
       data: serialized,
       success: function(){
+        $(".new-tweet textarea").val("")
+        // Calling loadTweets() again in case another user had tweeted between when the page was loaded and a new tweet was submitted
         loadTweets();
       }
     })}
