734a995f8f7bf4be77e92d9774b33d366616b06c
Lukas Horak <lukasbhorak@gmail.com>
2019-06-12 17:48:18 -0400

    refactors for efficiency/readability

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,17 +7,18 @@ function escape(str) {
 createTweetElement = (tweetObj) => {
   // Template Literal
   const articleText = `
-  <header>
-    <div class="profile"><img src="${escape(tweetObj.user.avatars.regular)}">
-      <h2>${escape(tweetObj.user.name)}</h2>
-    </div>
-    <span class="handle">${escape(tweetObj.user.handle)}</span>
-  </header>
-  <p class="tweet-content">${escape(tweetObj.content.text)}</p>
-  <footer>
-    <p class="timestamp">${timeAgo(escape(tweetObj.created_at))}</p>
-    <div class="like-share"><i class="fa fa-flag"></i><i class="fa fa-retweet"></i><i class="fa fa-heart"></i></div>
-  </footer>`
+    <header>
+      <div class="profile"><img src="${escape(tweetObj.user.avatars.regular)}">
+        <h2>${escape(tweetObj.user.name)}</h2>
+      </div>
+      <span class="handle">${escape(tweetObj.user.handle)}</span>
+    </header>
+    <p class="tweet-content">${escape(tweetObj.content.text)}</p>
+    <footer>
+      <p class="timestamp">${timeAgo(escape(tweetObj.created_at))}</p>
+      <div class="like-share"><i class="fa fa-flag"></i><i class="fa fa-retweet"></i><i class="fa fa-heart"></i></div>
+    </footer>
+  `
 
   return $('<article>').addClass("tweet").html(articleText)
 };
@@ -28,11 +29,9 @@ const renderTweets = (jeffData, tweets) => {
   for (let tweet of jeffData) {
     // Use an IIFE to render tweets one at a time
     let index = jeffData.indexOf(tweet);
-    (function (i) {
-      setTimeout(function () {
-        tweets.append(createTweetElement(tweet).fadeIn(700));
-      }, 50 * i);
-    })(index);
+    setTimeout(function () {
+      tweets.append(createTweetElement(tweet).fadeIn(700));
+    }, 50 * index);
   }
 }
 
