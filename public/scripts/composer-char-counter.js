dd0ec0f44a9908a73254be1c3f744b36ac3558a4
Lukas Horak <lukasbhorak@gmail.com>
2019-06-12 12:12:20 -0400

    quote consistency changes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-  $('.new-tweet textarea').on("input", function(){
+  $('.new-tweet textarea').on('input', function(){
     const inputLength = $(this).val().length;
     const charsLeft = 140 - inputLength;
     
-    // Red for over, yellow for < 10
-    const status = charsLeft < 0 ? "over" : charsLeft < 10 ? "warn" : "under";
-    $(this).siblings('.counter').text(charsLeft).attr("data-status", status);
+    // Red for over, goldenrod for < 10
+    const status = charsLeft < 0 ? 'over' : charsLeft < 10 ? 'warn' : 'under';
+    $(this).siblings('.counter').text(charsLeft).attr('data-status', status);
   })
 });
